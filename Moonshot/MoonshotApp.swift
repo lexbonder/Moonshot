7e53dd6f040cacbc021a35b918f81d14d282ac64
Alex Bonder <lexbonder@gmail.com>
2023-07-27 07:43:28 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Alex Bonder on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
